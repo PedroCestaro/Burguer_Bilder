bfbcc490fa8c01e0d17c86ddbfc1e5de91700b41
Pedro Cestaro <pedrocmanoel@gmail.com>
2020-08-13 17:14:59 -0300

    adequando o Burguer Component

--- Burguer/burguer-builder/src/components/Burguer/Burguer.js
+++ Burguer/burguer-builder/src/components/Burguer/Burguer.js
@@ -3,11 +3,21 @@ import BurguerIngredient from './BurguerIngredient/BurguerIngredient'
 import './Burguer.css';
 
 const burguer =(props) => {
+    /*keys() = got the keys of an object and turn into an array*/
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => { /*now that the ingredients is an array, to manipulate the values, using map*/
+            return [...Array(props.ingredients[igKey])]/*JavaScript method, to create the array with the ingredients length. For eahch igKey, is adding a term in the array*/
+                .map((_, i) => {/*accessing the index of the arrays*/
+                 return <BurguerIngredient key={igKey + i} type={igKey}/>
+                    console.log(igKey);
+                });
+        
+        });
+
     return(
         <div className="burguer">
             <BurguerIngredient type="bread-top"/>
-            <BurguerIngredient type="cheese"/>
-            <BurguerIngredient type="meat"/>
+                {transformedIngredients}
             <BurguerIngredient type="bread-bottom"/>
         </div>
     );
--- Burguer/burguer-builder/src/containers/BurguerBilder/BurguerBilder.js
+++ Burguer/burguer-builder/src/containers/BurguerBilder/BurguerBilder.js
@@ -4,10 +4,20 @@ import Burguer from '../../components/Burguer/Burguer'
 
 class BurguerBilder extends Component{
     /*State will be manioulated here, 'cause, in this papge is where it's shows*/
+    state = {/*adding indexes (igKey) inside the transformedIngredients Array*/
+        ingredients:{
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat:2
+        }
+    }
+    
     render() {
         return(
            <Aux>
-               <Burguer/>
+               <Burguer ingredients={this.state.ingredients/* adc state to the app*/}/>
+               <div>Build Controls</div>
            </Aux>
         );
     }
